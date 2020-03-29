454d78d3df34c617bb69f4396745dfc4180d32e4
xieguojun <773417250@qq.com>
2020-03-29 17:44:08 +0800

    添加一些注释

--- git-test-project/src/cn/tedu/git/First.java
+++ git-test-project/src/cn/tedu/git/First.java
@@ -7,9 +7,11 @@ public class First {
         System.out.println("hello World");
         System.out.println("hello World");
         System.out.println("hello World");
+        //helloworld
     }
     public void test(){
         int i =0;
         i += 10;
+        System.out.println(i);
     }
 }
