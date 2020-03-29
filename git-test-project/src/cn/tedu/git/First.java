1035f2fc75c84ffbe73f2842b172a6dc7ffcc78e
xieguojun <773417250@qq.com>
2020-03-29 17:01:04 +0800

    从idea中将工程提交到本地仓库

---
+++ git-test-project/src/cn/tedu/git/First.java
@@ -0,0 +1,11 @@
+package cn.tedu.git;
+
+public class First {
+    public void sayHello(){
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+    }
+}
